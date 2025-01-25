9d16075598fddeb1c97b34d090ca09458629ed7c
yashThinkwik <148205995+yashThinkwik@users.noreply.github.com>
2025-01-25 17:18:52 -0500

    Added Custom Popups (Jasjit)
    - Due to some conflicts adding code for login and update Popup from my system

--- src/View/LoginPopup.js
+++ src/View/LoginPopup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../Style/LoginPopup.css'; // Add your styles in this CSS file
 import CustomButton from '../Controller/CustomButton'; // Import the Button component
 import TabButton from '../Controller/TabButton'; // Import the Tab Button component
-
+import AlertPopup from '../Controller/AlertPopup.js'; // Import the Alert popup component
 function LoginPopup({ isOpen, onClose, onLoginSuccess }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -10,16 +10,14 @@ function LoginPopup({ isOpen, onClose, onLoginSuccess }) {
   const [activeTab, setActiveTab] = useState('login'); // 'login' or 'signup'
   const [showPassword, setShowPassword] = useState(false); // State for password visibility toggle
   const [showConfirmPassword, setShowConfirmPassword] = useState(false); // Separate state for confirm password visibility
-
-  // hardcoded login credentials 
+  const [alert, setAlert] = useState(''); // State for error message
+  // hardcoded login credentials
   const loginCredentials = {
     email: 'admin@gmail.com',
     password: 'password',
   };
-
   const handleLoginSubmit = (e) => {
     e.preventDefault();
-
     // Check if the entered credentials match the hardcoded credentials
     if (email === loginCredentials.email && password === loginCredentials.password) {
       console.log('Login successful');
@@ -27,35 +25,31 @@ function LoginPopup({ isOpen, onClose, onLoginSuccess }) {
       onClose(); // Close the popup after successful login
     } else {
       console.log('Invalid email or password');
-      alert('Invalid email or password. Please try again.');
+      setAlert('Invalid email or password. Please try again'); // Set the error message
     }
   };
-
   const handleSignUpSubmit = (e) => {
     e.preventDefault();
     console.log('Signing up with:', email, password);
     onClose(); // Close the popup after signup attempt
   };
-
   return (
     isOpen && (
       <div className="popup-overlay">
         <div className="popup-container">
           <h2>{activeTab === 'login' ? 'Login' : 'Sign Up'}</h2>
-          
           <div className="tabs">
-            <TabButton 
-              isActive={activeTab === 'login'} 
-              label="Login" 
-              onClick={() => setActiveTab('login')} 
+            <TabButton
+              isActive={activeTab === 'login'}
+              label="Login"
+              onClick={() => setActiveTab('login')}
             />
-            <TabButton 
-              isActive={activeTab === 'signup'} 
-              label="Sign Up" 
-              onClick={() => setActiveTab('signup')} 
+            <TabButton
+              isActive={activeTab === 'signup'}
+              label="Sign Up"
+              onClick={() => setActiveTab('signup')}
             />
           </div>
-
           {activeTab === 'login' ? (
             <form onSubmit={handleLoginSubmit}>
               <div className="form-group">
@@ -87,13 +81,12 @@ function LoginPopup({ isOpen, onClose, onLoginSuccess }) {
                   ></i>
                 </div>
               </div>
-             
               <CustomButton
                 title='Login'
-                backgroundColor='#01012c'
-                titleColor='#ffcc00'
-                hoverTitleColor='#01012c'
-                hoverBackgroundColor='#ffcc00'
+                backgroundColor='#01012C'
+                titleColor='#FFCC00'
+                hoverTitleColor='#01012C'
+                hoverBackgroundColor='#FFCC00'
                 height='50px'
                 width='150px'
               >
@@ -147,28 +140,25 @@ function LoginPopup({ isOpen, onClose, onLoginSuccess }) {
                     style={{ cursor: 'pointer' }}
                   ></i>
                 </div>
-
               </div>
-            
               <CustomButton
                 title='Sign Up'
-                backgroundColor='#01012c'
-                titleColor='#ffcc00'
-                hoverTitleColor='#01012c'
-                hoverBackgroundColor='#ffcc00'
+                backgroundColor='#01012C'
+                titleColor='#FFCC00'
+                hoverTitleColor='#01012C'
+                hoverBackgroundColor='#FFCC00'
                 height='50px'
                 width='150px'
               >
               </CustomButton>
             </form>
           )}
-
           <button onClick={onClose} className="cancel-btn">×</button>
-
+            {/* If there's an error, show the ErrorPopup */}
+            {alert && <AlertPopup message={alert} onClose={() => setAlert('')} />}
         </div>
       </div>
     )
   );
 }
-
 export default LoginPopup;
--- src/View/UpdateProfile.js
+++ src/View/UpdateProfile.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../Style/UpdateProfile.css'; // Add your styles here
 import CustomButton from '../Controller/CustomButton'; // Import the Button component
-
+import AlertPopup from '../Controller/AlertPopup.js'; // Import the Alert popup component
 function UpdateProfilePopup({ onClose }) {
   // Declare state variables for form inputs and password visibility toggles
   const [email, setEmail] = useState('');
@@ -11,25 +11,26 @@ function UpdateProfilePopup({ onClose }) {
   const [showoldPassword, setShowOldPassword] = useState(false);
   const [shownewPassword, setShowNewPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-
+  const [alert, setAlert] = useState(''); // State for error message
   // Handle form submission (you can add validation and API call logic here)
   const handleSubmit = (e) => {
     e.preventDefault();
     if (newpassword !== confirmPassword) {
-      alert('Passwords do not match');
-      return;
-    }
-
+        setAlert('Passwords do not match'); // Set the error message
+        return;
+      }
     if(oldpassword !== "password"){
-        alert('Old Password is incorrect');
+        setAlert('Old Password is Incorrect'); // Set the error message
         return;
     }
     // Proceed with updating profile logic (API call, etc.)
-    alert('Profile updated successfully!');
+    setAlert('Profile updated successfully!');
     onClose(); // Close the popup after successful update
   };
-
   return (
+    <div>
+    {/* If there's an error, show the ErrorPopup */}
+    {alert && <AlertPopup message={alert} onClose={() => setAlert('')} />}
     <div className="popup-overlay">
       <div className="popup-container">
         <h2>Update Profile</h2>
@@ -46,8 +47,6 @@ function UpdateProfilePopup({ onClose }) {
               placeholder="Enter your email"
             />
           </div>
-          
-           
           <div className="form-group">
             <label htmlFor="password">Old Password</label>
             <div className="password-container">
@@ -59,10 +58,8 @@ function UpdateProfilePopup({ onClose }) {
                 required
                 placeholder="Enter your password"
               />
-              
             </div>
           </div>
-
           <div className="form-group">
             <label htmlFor="password">New Password</label>
             <div className="password-container">
@@ -81,7 +78,6 @@ function UpdateProfilePopup({ onClose }) {
               ></i>
             </div>
           </div>
-
           <div className="form-group">
             <label htmlFor="confirmPassword">Confirm New Password</label>
             <div className="password-container">
@@ -100,23 +96,20 @@ function UpdateProfilePopup({ onClose }) {
               ></i>
             </div>
           </div>
-          
           <CustomButton
             title='Update'
-            backgroundColor='#01012c'
-            titleColor='#ffcc00'
-            hoverTitleColor='#01012c'
-            hoverBackgroundColor='#ffcc00'
+            backgroundColor='#01012C'
+            titleColor='#FFCC00'
+            hoverTitleColor='#01012C'
+            hoverBackgroundColor='#FFCC00'
             height='50px'
             width='150px'
           />
         </form>
-
-       
         <button onClick={onClose} className="cancel-btn">×</button>
       </div>
     </div>
+    </div>
   );
 }
-
 export default UpdateProfilePopup;
