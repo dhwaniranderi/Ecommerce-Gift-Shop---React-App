4149f90fa128330b71462a4f2b099e76be6a1382
yashThinkwik <148205995+yashThinkwik@users.noreply.github.com>
2025-01-25 21:39:29 -0500

    Added Product details
    - Added Product details page

--- src/Controller/Product.js
+++ src/Controller/Product.js
@@ -1,11 +1,18 @@
-// Product.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../Style/Product.css'; // Add your styles in this CSS file
 import Button from '../Controller/CustomButton'; // Import the Button component
 
-function Product({ name, description, imgSrc, price, handleBuyNow }) {
+function Product({ id, name, description, imgSrc, price, handleBuyNow }) {
+  const navigate = useNavigate();
+
+  // Handle product click to navigate to ProductDetails page
+  const handleProductClick = () => {
+    navigate(`/product/${id}`);
+  };
+
   return (
-    <div className="product">
+    <div className="product" onClick={handleProductClick}>
       <img src={imgSrc} alt={name} />
       <h3>{name}</h3>
       <p>{description}</p> {/* This will display the description */}
